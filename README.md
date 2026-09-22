477d0d7d9d104acd6fd9e72d7b17155ad1b601b6
Yapeng Su <1186179435@qq.com>
2026-09-22 12:01:51 +0800

    feat: 去除frontend脚手架的ts的langgraph后端， 添加全局的pnpm启动服务

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# 后端
-- langgraph new backend --template new-langgraph-project-python
+# 后端Python
+- 脚手架：langgraph new backend --template new-langgraph-project-python
 
 可选  
 - deep-agent-python  1111
@@ -8,5 +8,12 @@
 - new-langgraph-project-python  dddd
 - new-langgraph-project-js
 
-# 前端
-- npx create-assistant-ui@latest -t langchain
+
+- langsmith调试：https://smith.langchain.com/studio/?baseUrl=http://127.0.0.1:2024
+
+# 前端Typescript
+- 脚手架：npx create-assistant-ui@latest -t langchain
+
+
+# 配置环境参数
+.env
--- frontend/components/thread.aui.tsx
+++ frontend/components/thread.aui.tsx
@@ -4,9 +4,9 @@ import {
   ComposerAddAttachment,
   ComposerAttachments,
   UserMessageAttachments,
-} from "@/components/assistant-ui/elements/attachment.aui";
+} from "@/components/attachment.aui";
 import { File } from "@/components/file";
-import { ThreadFollowupSuggestions } from "@/components/assistant-ui/elements/follow-up-suggestions.aui";
+import { ThreadFollowupSuggestions } from "@/components/follow-up-suggestions.aui";
 import { Image } from "@/components/image";
 import { MarkdownText } from "@/components/markdown-text";
 import {
@@ -15,13 +15,13 @@ import {
   ReasoningRoot,
   ReasoningText,
   ReasoningTrigger,
-} from "@/components/assistant-ui/elements/reasoning.aui";
-import { ToolFallback } from "@/components/assistant-ui/elements/tool-fallback.aui";
+} from "@/components/reasoning.aui";
+import { ToolFallback } from "@/components/tool-fallback.aui";
 import {
   ToolGroupContent,
   ToolGroupRoot,
   ToolGroupTrigger,
-} from "@/components/assistant-ui/elements/tool-group.aui";
+} from "@/components/tool-group.aui";
 import { TooltipIconButton } from "@/components/tooltip-icon-button";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
